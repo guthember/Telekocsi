00eae03ed37fd45b7645fa922ec0105814622753
guthember <guthember@gmail.com>
2020-10-21 08:33:53 +0200

    Osztályok elkészítése

---
+++ Telekocsi/Auto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telekocsi
+{
+  class Auto
+  {
+    public string Indulas { get; private set; }
+    public string Cel { get; private set; }
+    public string Rendszam { get; private set; }
+    public string Tel { get; private set; }
+    public int Ferohely { get; private set; }
+
+    public Auto(string indulas, string cel, string rendszam, string tel, int ferohely)
+    {
+      Indulas = indulas;
+      Cel = cel;
+      Rendszam = rendszam;
+      Tel = tel;
+      Ferohely = ferohely;
+    }
+  }
+}
---
+++ Telekocsi/Igeny.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telekocsi
+{
+  class Igeny
+  {
+    public string Azonosito { get; private set; }
+    public string Indulas { get; private set; }
+    public string Cel { get; private set; }
+    public int Szemelyek { get; private set; }
+
+    public Igeny(string azonosito, string indulas, string cel, int szemelyek)
+    {
+      Azonosito = azonosito;
+      Indulas = indulas;
+      Cel = cel;
+      Szemelyek = szemelyek;
+    }
+  }
+}
